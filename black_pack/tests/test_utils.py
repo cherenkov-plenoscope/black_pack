b472e1ee77c5e7644bd0bc7f90fa15349a44f4eb
Sebastian Achim Mueller <sebastian-achim.mueller@mpi-hd.mpg.de>
2023-09-11 11:48:36 +0200

    more tests

--- black_pack/tests/test_utils.py
+++ black_pack/tests/test_utils.py
@@ -5,3 +5,8 @@ def test_has_any_upper():
     assert not black_pack.has_any_upper("")
     assert not black_pack.has_any_upper("abc")
     assert not black_pack.has_any_upper("123")
+    assert black_pack.has_any_upper("A")
+    assert black_pack.has_any_upper("aA")
+    assert black_pack.has_any_upper("Aa")
+    assert black_pack.has_any_upper("ABC")
+    assert black_pack.has_any_upper("awe123ABC")
